ce89fb710a1c74b03fb974bbfd61995af1311744
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-09-04 19:07:31 -0400

    Add build script

---
+++ docker-build.sh
@@ -0,0 +1,15 @@
+name="demo"
+
+# Look for a line akin to: "version - '0.0.1-SNAPSHOT'"
+# Only take the '0.0.1-SNAPSHOT' part
+version=$(cat build.gradle | grep "version = " | cut -d" " -f3)
+
+# Remove quotes
+version="${version%\'}"
+version="${version#\'}"
+
+tag="$name:$version"
+
+echo "Building image:\t$tag"
+
+docker build -t $tag --build-arg name="$name" --build-arg version="$version" .
